1aacb4fb226e5132ff66f1bedaa686accf64a45a
Xintong Li <x969li@edu.uwaterloo.ca>
2021-01-09 01:57:39 -0500

    data_processing.py debug recipes.pkl modified

--- data/data_processing.py
+++ data/data_processing.py
@@ -9,9 +9,11 @@ recipes["id"] = [i for i in range(1, len(recipes) + 1)]
 def split_strings(strin):
     s = str(strin).replace("[", "")
     s = s.replace("]", "")
-    return s.split(",")
+    return s.split(", ")
 
 recipes["ingredients"] = recipes["ingredients"].apply(split_strings)
+recipes["course"] = recipes["course"].apply(split_strings)
+recipes["cuisine"] = recipes["cuisine"].apply(split_strings)
 
 import joblib
 joblib.dump(recipes, "recipes.pkl")
