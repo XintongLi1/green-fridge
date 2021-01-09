961d0efd12bcc91feaedfe5f5320ce55f93eb69e
Xintong Li <x969li@edu.uwaterloo.ca>
2021-01-09 01:45:47 -0500

    refactor data

--- data/data_processing.py
+++ data/data_processing.py
@@ -6,7 +6,18 @@ recipes.drop("id", axis=1)
 
 recipes["id"] = [i for i in range(1, len(recipes) + 1)]
 
+def split_strings(strin):
+    s = str(strin).replace("[", "")
+    s = s.replace("]", "")
+    return s.split(",")
+
+recipes["ingredients"] = recipes["ingredients"].apply(split_strings)
+
 import joblib
 joblib.dump(recipes, "recipes.pkl")
 
 recipes = joblib.load("recipes.pkl")
+
+
+
+
